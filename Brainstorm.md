d1c14378fdc1c86771ec8ca9aad929be7ffe9034
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2026-03-28 13:55:16 +0200

    Initial Commit- Partial-Complete Script
    Created many objects and methods to complete phase 1 of the project:
    Tic Tac Toe game through the terminal.
    Progress:
    - Gameboard Object
    - Player Object
    - Game Object
    For further edits to be made to complete phase 1

---
+++ Brainstorm.md
@@ -0,0 +1,36 @@
+# Instructions
+[TOP's instructions for this project](INSTRUCTIONS.md)
+# Goal
+Have a working browser TicTacToe game for 2 players with basic functionality
+# Required Minimal Functionalities
+- Start game
+- Receive user inputs (names)
+- Display markers where players click
+- End game over 3 successions or draw
+- Show result after game end
+- Restart game
+# Desired Code
+Have the code to be inside objects, function factories, IIFEs, and reduce global variables and functions as much as possible.
+# Components
+For this project, the following components are required:
+- Player object
+    - player1 (name)
+    - player2 (name)
+- Gameboard object
+    - gameboard (array)
+    - markers (X, O)
+- Game object
+    - start (function)
+    - end game (function)
+    - show results (function)
+    - restart (function)
+
+Variables and functions must be inside these objects depending on the logic as much as possible.
+# Brainstorm
+This should be tackled in steps. First I should start with what TOP instructed, then try to figure out how to build each functionality and connect them little by little.
+
+First, the 3 main objects:
+- Player (with name and marker)
+- Gameboard (with array and marker)
+- Game (with game logic flow)
+
--- script.js
+++ script.js
@@ -1,1 +1,103 @@
-console.log("Hello, world!");
+// Gameboard object for gameboard-related functionality
+const Gameboard = (function () {
+    // Board variables
+    const gameBoard = [];
+    const boardLength = 9;
+    let markersPlaced = 0;
+    // Markers
+    const markerX = 'X';
+    const markerO = 'O';
+    // Players (hard-coded for now)
+    const player1 = Player("Nour", markerX);
+    const player2 = Player("Abdullah", markerO);
+    return {
+        // Reset all values in array
+        resetBoard: () => {
+            for (let i = 0; i < boardLength; i++) {
+                if (!(gameBoard[i] === undefined)) {
+                    gameBoard[i] = undefined;
+                }
+            }
+        },
+        // Log board index to prevent empty values
+        logBoard: (i) => {
+            const boardValue = gameBoard[i];
+            return boardValue;
+        },
+        // Logic to insert marker in array, array termination, and array iteration with edge cases
+        insertMarker: (index) => {
+            if (gameBoard[index] === undefined) {
+                if (markersPlaced % 2 == 0) {
+                    gameBoard[index] = player2.getMarker();
+                    console.log(gameBoard[index]);
+                    markersPlaced += 1
+                    if (markersPlaced <= 3) {
+                        Gameboard.checkResults();
+                    }
+                } else {
+                    gameBoard[index] = player1.getMarker();
+                    console.log(gameBoard[index]);
+                    markersPlaced += 1;
+                    if (markersPlaced <= 3) {
+                        Gameboard.checkResults();
+                    }
+                }
+            } else if ((gameBoard[index] === 'X') || (gameBoard[index] === 'O')){
+                console.log("Spot Already Marked");
+            } else {
+                return
+            }
+        },
+        checkResults: () => {
+            if (
+                (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) ||
+                (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) ||
+                (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) ||
+                (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) ||
+                (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) ||
+                (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8]) ||
+                (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) ||
+                (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6])
+            ) {
+                console.log("Game ends!");
+                Game.end();
+            }
+        }
+    }
+})();
+
+// Player object for player-related functionality
+function Player(playerName, playerMarker) {
+    const name = playerName;
+    const marker = playerMarker;
+    const getName = () => {
+        return name;
+    }
+    const getMarker = () => {
+        return marker;
+    }
+    return { getName, getMarker };
+}
+
+const Game = (function () {
+    return {
+        start: () => {
+            for (let i = 0; i < 9; i++) {
+                Gameboard.insertMarker(i);
+                if (Gameboard.logBoard(i) === undefined) {
+                    i -= 1
+                }
+            }
+        },
+        restart: () => {
+            Gameboard.resetBoard();
+            Game.start();
+        },
+        end: () => {
+            Gameboard.insertMarker(null);
+            Gameboard.resetBoard();
+        }
+    }
+})();
+
+Game.start();
